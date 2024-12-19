4f16c655828c895aca121034d7d451c62bb5e264
Mladen <mladinaki54@gmail.com>
2024-12-19 19:04:21 +0200

    Add connection string in mongobd atlas
    Add connection string in mongobd atlas

--- beckend/src/constant/constant.js
+++ beckend/src/constant/constant.js
@@ -1,10 +1,12 @@
 const constant = {
     PORT: 4000,
-    DB_URL: 'mongodb+srv://mladinaki:mladinaki123@cluster0.popn0lp.mongodb.net',
+    DB_URL: 'mongodb+srv://mladinaki:mladinaki123@e-commerce-shop.g9bes.mongodb.net/?retryWrites=true&w=majority&appName=E-Commerce-shop',
     JWT_SECRET_KEY: 'e763c24e-727d-11ee-b962-0242ac120002',
     JWT_SECRET: 'mladinaki',
     ADMIN_EMAIL: 'mladinaki54@gmail.com',
     ADMIN_PASSWORD: 'mladinaki78',
 }
 
-module.exports = constant
+module.exports = constant;
+
+
